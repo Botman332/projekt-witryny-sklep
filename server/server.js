c051fa7a4846dc88169726c8e5fb986b625ede0b
botman <botman332@gmail.com>
2023-09-14 18:05:07 +0200

    Dodano zmiany potrzebne do umieszczenia aplikacji na serwerze

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "UserTwo", "UserThree"] })
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")}) 
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
