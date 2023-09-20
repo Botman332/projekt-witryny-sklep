ed034fffc3ca0bfd90f6d3066a6ecf3b3c272180
botman332@gmail.com <you@example.com>
2023-09-20 09:26:22 +0200

    Dodano polaczenie z baza danych

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const mysql= require('mysql');
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -10,6 +11,28 @@ app.get("/api", (req, res) => {
 
 app.listen(5000, () => {console.log("Server started on port 5000")}) 
 
+const connection = mysql.createConnection({
+    host     : 'bvlmt5ugt9t6m7rdvtpm-mysql.services.clever-cloud.com',
+    user     : 'uyob3vcu3sbv6pgo',
+    password : 'dpuY0IRPVTnyGz64YrDs',
+    database : 'bvlmt5ugt9t6m7rdvtpm'
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + connection.threadId);
+  });
+  
+  connection.query('SELECT * FROM users', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+  });
+  
+  connection.end();
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 //   });
